719a501dc43652dee639ac7827d0a034f4bb26fe
teguia kevin <teguia@localhost.localdomain>
2017-01-19 18:48:16 +0000

    Initial commit

---
+++ resources/assets/js/components/message.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+
+        <div>this is template body</div>
+        <other-component/>
+    </div>
+</template>
+<style>
+    body{
+
+    }
+</style>
+<script>
+   // import HeaderComponent from './components/Example.vue'
+    import OtherComponent from './Example.vue'
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        components:{
+            'other-component':OtherComponent,
+
+        }
+    }
+</script>
---
+++ resources/assets/js/components/modal.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <a class="waves-effect waves-light btn" href="#modal1" @click="open">Modal</a>
+
+        <!-- Modal Structure -->
+        <div id="modal1" class="modal">
+            <div class="modal-content">
+                <h4>Modal Header</h4>
+                <p>A bunch of text</p>
+            </div>
+            <div class="modal-footer">
+                <a href="#!" class=" waves-effect waves-green btn-flat" @click="close">Agree</a>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+
+    export default{
+        data(){
+            return{
+                msg:'hello vue'
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+            open(){
+                $('#modal1').modal();
+            },
+            close(){
+                 $('#modal1').modal();
+            }
+        }
+    }
+
+
+
+</script>
